b2c53154a53523ddfc44312753c9e98627dd45e8
connieksun <71198240+connieksun@users.noreply.github.com>
2023-03-12 13:49:23 -0700

    implement traditional IRA, update with catchup contributions

--- calculators/ira.js
+++ calculators/ira.js
@@ -1,17 +1,31 @@
-let annualContribution = 6500;
-let annualReturn = .06;
-let currentIRABalance = 13000;
-
+const catchupAge = 50;
 // Formula from https://www.wallstreetmojo.com/roth-ira-calculator/
 // Verified with https://www.dinkytown.net/java/roth-ira-calculator.html
-function calculateRothIRA(){
-    let yearsInterest = ageOfRetirement - currentAge;
-    let compoundMult = (1 + annualReturn)**yearsInterest;
-    let total = currentIRABalance * compoundMult +
-               annualContribution * ((compoundMult - 1) * (1 + annualReturn) / annualReturn);
-    console.log("Roth IRA: $" + Math.round(total));
+function calculateRothIRA(curBal, annCont, annRet, catchupCont){
+    let total = curBal;
+    let compoundMult = 1 + annRet;
+    for (let i = currentAge; i < ageOfRetirement; i++) {
+        total = total * compoundMult + annCont * compoundMult;
+        if (i >= catchupAge) {
+            total += catchupCont * compoundMult;
+        }
+    }
+    // formula without loop
+    // let yearsInterest = ageOfRetirement - currentAge;
+    // let compoundMult = (1 + annualReturn)**yearsInterest;
+    // let total = currentIRABalance * compoundMult +
+    //            annualContribution * ((compoundMult - 1) * (1 + annualReturn) / annualReturn);
     return total;
 }
 
+// TODO: replace with tax info from taxes.js
+let retirementTaxRate = .15;
+// Verified with https://www.dinkytown.net/java/traditional-ira-calculator.html
+function calculateTraditionalIRA(curBal, annCont, annRet, catchupCont){
+    let total = calculateRothIRA(curBal, annCont, annRet, catchupCont);
+    total *= 1 - retirementTaxRate;
+    return total; // total after taxes
+}
 
-calculateRothIRA();
+console.log("Roth IRA $" + Math.round(calculateRothIRA(13000, 6500, .06, 1000)));
+console.log("Traditional IRA $" + Math.round(calculateTraditionalIRA(13000, 6500, .06, 1000)));
