0c9a56c7dda425c53530f7ef16f6ac4cdc35cb69
Amy Paul <alpshadow@gmail.com>
2023-03-16 23:15:20 -0700

    Bug fixes and finished tax calculation

--- calculators/savings.js
+++ calculators/savings.js
@@ -3,13 +3,14 @@ let savingsAPY = 0.0001; // Ew
 let highYieldSavingsAPY = 0.03;
 let contributionMax = null;
 let currentSavingsBalance = 1000;
+let portionOfSalaryToContributeSavings = .1; 
 
 function calculateSavings() {
     let total = currentSavingsBalance;
     let currentSalary = salary;
 
     for  (let i = 0; i < (ageOfRetirement - currentAge); i++) {
-        let amountAddedThisYear = currentSalary * portionOfSalaryToContribute;
+        let amountAddedThisYear = currentSalary * portionOfSalaryToContributeSavings;
         let returnOnTotal = total * savingsAPY;
         total = total + amountAddedThisYear + returnOnTotal;
         currentSalary = currentSalary*(1 + annualSalaryIncrease);
