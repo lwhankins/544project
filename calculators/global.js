655a600b1319c46d4ca73fa9ad4860a2abcda96f
Luke Hankins <lwhankins@email.arizona.edu>
2023-03-20 20:12:04 -0700

    Linked slider/input onchange to global variables

--- calculators/global.js
+++ calculators/global.js
@@ -3,3 +3,23 @@ var ageOfRetirement = 65;
 var yearsInRetirement = 20;
 var salary = 50000;
 var annualSalaryIncrease = .03;
+
+function setAge(age) {
+    currentAge = parseInt(age);
+}
+
+function setAgeOfRetirement(ageOfRet) {
+    ageOfRetirement = parseInt(ageOfRet);
+}
+
+function setYearsInRetirement(yearsInRet) {
+    yearsInRetirement = parseInt(yearsInRet);
+}
+
+function setSalary(sal) {
+    salary = parseInt(sal);
+}
+
+function setSalaryIncrease(salaryIncrease) {
+    annualSalaryIncrease = parseFloat(salaryIncrease);
+}
--- project.js
+++ project.js
@@ -2,29 +2,31 @@ let ageGroup = d3.select("#basic_info")
                   .append("g")
                   .attr("class", "param")
                   .attr("id", "age")
-makeInputSlider(ageGroup, "Age", 10, 80, 30, step=1);
+makeInputSlider(ageGroup, "Age", 10, 80, 30, step=1, format=",", setAge);
 
 let retirementAgeGroup = d3.select("#basic_info")
                   .append("g")
                   .attr("class", "param")
                   .attr("id", "retirementAge")
-makeInputSlider(retirementAgeGroup, "Retirement Age", 30, 80, 65, step=1);
+makeInputSlider(retirementAgeGroup, "Retirement Age", 30, 80, 65, step=1, format=",", setAgeOfRetirement);
 
 let retirementLengthGroup = d3.select("#basic_info")
                   .append("g")
                   .attr("class", "param")
                   .attr("id", "retirementLength")
-makeInputSlider(retirementLengthGroup, "# Years in Retirement", 5, 70, 20, step=1);
+makeInputSlider(retirementLengthGroup, "# Years in Retirement", 5, 70, 20, step=1, format=",", setYearsInRetirement);
 
 
-let currentSalary = d3.select("#basic_info")
+let currentSalaryGroup = d3.select("#basic_info")
                   .append("g")
                   .attr("class", "param")
                   .attr("id", "currentSalary")
-makeInputSlider(currentSalary, "Current Salary", 15000, 500000, 50000, step=1000);
+makeInputSlider(currentSalaryGroup, "Current Salary", 15000, 500000, 50000, step=1000, format=",", setSalary);
 
-let averageAnnualRaise = d3.select("#basic_info")
+let averageAnnualRaiseGroup = d3.select("#basic_info")
                   .append("g")
                   .attr("class", "param")
                   .attr("id", "averageAnnualRaise")
-makeInputSlider(averageAnnualRaise, "Average Salary Raise Per Year", 0, .10, .03, step=.005, format='.1%');
+makeInputSlider(averageAnnualRaiseGroup, "Average Salary Raise Per Year", 0, .10, .03, step=.005, format='.1%', setSalaryIncrease);
+
+
