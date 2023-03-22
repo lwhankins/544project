2dcb05d4b2cc749d75fc8df6e3ba3d410bf2fc2b
connieksun <71198240+connieksun@users.noreply.github.com>
2023-03-21 23:33:04 -0700

    partial implementation of makeAccountDiv()

--- generators/utils.js
+++ generators/utils.js
@@ -1,10 +1,12 @@
-let padding = 30;
-let width = 800;
-let height = 400;
+let padding = 10;
+let width = 600;
+let height = 100;
 // use d3-simple-slider from https://github.com/johnwalley/d3-simple-slider
-function makeInputSlider(parent, name, min, max, suggested, step=1, format=",", setGlob){
+function makeInputSlider(parent, name, min, max, suggested, step, format, setGlob){
+    // label is the top level holder
     let label = parent.append("label")
                       .text(name);
+    // label holds a div with the input (field where user inputs the value and presses up/down)
     let input = label.append("div")
                     .attr("class", "param-input")
                     .append("input")
@@ -14,6 +16,7 @@ function makeInputSlider(parent, name, min, max, suggested, step=1, format=",", 
                     .attr("min", min)
                     .attr("max", max)
                     .attr("step", step);
+    // label holds a div with the slider, contained in an svg
     var svg = label.append("div")
                     .attr("class", "param-slider")
                     .append("svg")
@@ -40,28 +43,52 @@ function makeInputSlider(parent, name, min, max, suggested, step=1, format=",", 
     });
 }
 
+function togglePanel(checkbox){
+    console.log(checkbox.property("checked"))
+}
 
-let group1 = d3.select("#calcs")
-                  .append("g")
-                  .attr("class", "param")
-                  .attr("id", "401kContributions")
-makeInputSlider(group1, "401(k) Contribution", .01, .20, .10, step=0.005, format='.1%');
+function addParam(parentDiv, config) {
+    let g = parentDiv.append("g")
+        .attr("class", "param"); // add id as well?
 
-let group2 = d3.select("#calcs")
-                .append("g")
-                .attr("class", "param")
-                .attr("id", "rothContributions")
-makeInputSlider(group2, "Roth Contribution", 0, 7000, 6500, step=100)
+    makeInputSlider(g, config.name, config.min, config.max, config.suggested,
+                    config.step, config.format, config.setGlob);
+}
 
-let group3 = d3.select('#calcs')
-                .append("g")
-                .attr("class", "param")
-                .attr("id", "cdContributions");
-makeInputSlider(group3, "CD Contributions", 0, 7000, 6500, step=100);
+let accountsDiv = d3.select("#accounts"); // matches index.html
+function makeAccountDiv(title, paramConfigs) {
+    // each account div is a container, the top level holder
+    let accountDiv = accountsDiv.append("div")
+        .attr("class", "container");
+    // container holds div for panel header, which is always shown
+    // panel header contains account name, money contributed, and toggle
+    let header = accountDiv.append("div")
+        .attr("class", "panel-header");
+    header.append("h3")
+        .attr("class", "header-title")
+        .text(title);
+    
+    // toggle is a label holding the checkbox
+    let toggleLabel = header.append("label")
+        .attr("class", "toggle");
+    let checkbox = toggleLabel.append("input")
+        .attr("type", "checkbox");
+    checkbox.on("change", () => togglePanel(checkbox));
+    toggleLabel.append("span") // TODO: styling
+        .attr("class", "slider");
+    
+    header.append("h3")
+        .attr("class", "header-amount")
+        .text("$ X,XXX");
+    
+    // container holds div for panel, which is shown if toggle is on
+    let panel = accountDiv.append("div")
+        .attr("class", "panel");
 
-let group4 = d3.select("#calcs")
-                .append("g")
-                .attr("class", "param")
-                .attr("id", "savingsContributions");
-makeInputSlider(group4, "Savings Contributions", 0, 7000, 6500, step=100);
+    
+    for (let i = 0; i < paramConfigs.length; i++) {
+        addParam(panel, paramConfigs[i]);
+    }
+    return accountDiv;
+}
 
