22332da492680057563d29808f521f32dc484636
connieksun <71198240+connieksun@users.noreply.github.com>
2023-03-23 00:56:47 -0700

    update traditional IRA and naming

--- calculators/ira.js
+++ calculators/ira.js
@@ -1,50 +1,77 @@
 const catchupAge = 50;
-let rothCurBal = 0;
-let rothAnnCont = 6000;
-let rothAnnRet = 0.07;
-let rothCatchupCont = 1000;
+let rothIraCurBal = 0;
+let rothIraAnnCont = 6000;
+let rothIraAnnRet = 0.07;
+let rothIraCatchupCont = 1000;
+
+let tradIraCurBal = 0;
+let tradIraAnnCont = 6000;
+let tradIraAnnRet = 0.07;
+let tradIraCatchupCont = 1000;
+
 // Formula from https://www.wallstreetmojo.com/roth-ira-calculator/
 // Verified with https://www.dinkytown.net/java/roth-ira-calculator.html
-function calculateRothIRA(){
-    let total = rothCurBal;
-    let compoundMult = 1 + rothAnnRet;
+function calculateIraHelper(curBal, annCont, annRet, catchupCont) {
+    let total = curBal;
+    let compoundMult = 1 + annRet;
     for (let i = currentAge; i < ageOfRetirement; i++) {
-        total = total * compoundMult + rothAnnCont * compoundMult;
+        total = total * compoundMult + annCont * compoundMult;
         if (i >= catchupAge) {
-            total += rothCatchupCont * compoundMult;
+            total += catchupCont * compoundMult;
         }
     }
     // formula without loop
     // let yearsInterest = ageOfRetirement - currentAge;
     // let compoundMult = (1 + annualReturn)**yearsInterest;
-    // let total = currentIRABalance * compoundMult +
+    // let total = currentIraBalance * compoundMult +
     //            annualContribution * ((compoundMult - 1) * (1 + annualReturn) / annualReturn);
     return total;
 }
+function calculateRothIra(){
+    return calculateIraHelper(rothIraCurBal, rothIraAnnCont, rothIraAnnRet, rothIraCatchupCont);
+}
 
 // Verified with https://www.dinkytown.net/java/traditional-ira-calculator.html
-function calculateTraditionalIRA(){
-    let total = calculateRothIRA();
+function calculateTraditionalIra(){
+    let total = calculateIraHelper(tradIraCurBal, tradIraAnnCont, tradIraAnnRet, tradIraCathcupCont);
     let retirementTaxAmount = taxesPerYear(total / yearsInRetirement);
     total -= (retirementTaxAmount * yearsInRetirement); // could update to change by year
     return total; // total after taxes
 }
 
-console.log(calculateTraditionalIRA());
+console.log(calculateTraditionalIra());
 
-function setRothCurBal(bal) {
+// roth IRA setters
+function setRothIraCurBal(bal) {
     rothCurBal = parseInt(bal);
 }
 
-function setRothAnnCont(cont) {
+function setRothIraAnnCont(cont) {
     rothAnnCont = parseInt(cont);
 }
 
-function setRothAnnRet(ret) {
+function setRothIraAnnRet(ret) {
     rothAnnRet = parseFloat(ret);
 }
 
-function setRothCatchupCont(cont){
+function setRothIraCatchupCont(cont){
     rothCatchupCont = parseInt(cont);
+}
+
+// traditional IRA setters
+function setTradIraCurBal(bal) {
+    tradIraCurBal = parseInt(bal);
+}
+
+function setTradIraAnnCont(cont) {
+    tradIraAnnCont = parseInt(cont);
+}
+
+function setTradIraAnnRet(ret) {
+    tradIraAnnRet = parseFloat(ret);
+}
+
+function setTradIraCatchupCont(cont){
+    tradIraCatchupCont = parseInt(cont);
 }
 
