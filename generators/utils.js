c7005144c7ce0cd44e5f9dfcd0337511761403ce
Amy Paul <alpshadow@gmail.com>
2023-04-23 14:37:37 -0700

    Finished account number graph

--- generators/utils.js
+++ generators/utils.js
@@ -638,7 +638,7 @@ function makeComparisonDiv(configs) {
     let carouselInner = carousel.append("div").attr("class", "carousel-inner");
     generateTable(carouselInner, configs);
     generateNumGraph(carouselInner);
-    generateAgeGraph(carouselInner);
+    generateAgeGraph(carouselInner)
 
     let indicators = carousel.append("div").attr("class", "carousel-indicators");
     indicators.append("button")
